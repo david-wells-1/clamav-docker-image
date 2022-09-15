985a76160f010d0a6a347ce44a2c1c512b79ff68
David Wells <david@wells-it.co.uk>
2022-09-15 21:29:23 +0100

    clamav-docker-image

--- src/consumer.js
+++ src/consumer.js
@@ -16,8 +16,10 @@ const app = Consumer.create({
     console.log('message', message);
     const parsedBody = JSON.parse(message.Body);
     const scanBucket = parsedBody.Records[0].s3.bucket.name
-    const documentKey = parsedBody.Records[0].s3.object.key.replace(/\+/g, " ");
+    // const documentKey = parsedBody.Records[0].s3.object.key.replace(/\+/g, " ");
     
+    const documentKey = parsedBody.Records[0].s3.object.key.replace(/+/g, " ").replace(/%28/g, "(").replace(/%29/g, ")");
+
     const { Body: fileData } = await s3.getObject({
       Bucket: scanBucket,
       Key: documentKey
